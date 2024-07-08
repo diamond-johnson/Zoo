89318eb88ef82b626fa3837ee605f1491029b3f6
diamond-johnson <diamond716729@gmail.com>
2024-07-08 18:26:07 +0330

    Create Giraffe.java

---
+++ Giraffe.java
@@ -0,0 +1,14 @@
+package workshop6;
+
+public class Giraffe extends Animal{
+	Giraffe(String Name, String Species) {
+		super(Name, Species);
+		// TODO Auto-generated constructor stub
+	}
+	public void whatIEat() {
+		System.out.println("I eat Plants.");
+	}
+	public void whereIStay() {
+		System.out.println("I stay in grass land.");
+	}
+}
