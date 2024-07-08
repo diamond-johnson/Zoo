bb087737473c5d8f0004139303f49227e3e61de4
diamond-johnson <diamond716729@gmail.com>
2024-07-08 18:26:35 +0330

    Create Tyrex.java

---
+++ Tyrex.java
@@ -0,0 +1,14 @@
+package workshop6;
+
+public class Tyrex extends Animal{
+	Tyrex(String Name, String Species) {
+		super(Name, Species);
+		// TODO Auto-generated constructor stub
+	}
+	public void whatIEat() {
+		System.out.println("I eat other dinasaurs.");
+	}
+	public void whereIStay() {
+		System.out.println("I stay in land.");
+	}
+}
