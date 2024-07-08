488e7f6e1deefaa09ad725b8e3893c87b362e929
diamond-johnson <diamond716729@gmail.com>
2024-07-08 18:26:20 +0330

    Create Lion.java

---
+++ Lion.java
@@ -0,0 +1,14 @@
+package workshop6;
+
+public class Lion extends Animal{
+	Lion(String Name, String Species) {
+		super(Name, Species);
+		// TODO Auto-generated constructor stub
+	}
+	public void whatIEat() {
+		System.out.println("I eat Zebra and wildebeest.");
+	}
+	public void whereIStay() {
+		System.out.println("I stay in desert.");
+	}
+}
